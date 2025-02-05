14b89d4ef0df8729fdd87e63e9da7bde8aecba5e
Omer Gemalmayan <ogemalmayan@gmail.com>
2025-02-05 17:14:49 +0300

    changed my part entirely
    because the one in the lab sucked

--- App.java
+++ App.java
@@ -2,80 +2,32 @@ public class App {
     
     public static void main(String[] args) 
     {
-        Scanner in = new Scanner(System.in);
-        int choice;
-        do 
-        {
-            System.out.printf("%s%n%s%n%s%n%s%n%s%n", "1. Find maximum number." 
-                                     , "2. Find minimum number."
-                                     , "3. Find thw avarage of the array." 
-                                     , "4. Find the sum of elements with odd- and even-numbered indexes."
-                                     , "5. Exit.");
-            System.out.print("Please choose an option: ");
-            choice = in.nextInt();
-            System.out.println("");
-            in.nextLine();
-            switch (choice) 
-            {
-                case 1:
-                    findMax(arr);
-                    break;
-                
-                case 2:
-                    findMin(arr);
-                    break;
-                case 3:
-                    arrayAverage(arr);
-                    break;
-                case 4:
-                    findOddEvenSumTotal(arr);
-                    break;
-                case 5:
-                    System.out.println("Exiting program...");
-                    break;
-                default:
-                    break;
-            }
-        } while(choice != 5);
 
     }
 
-    public static void findOddEvenSumTotal(int[] array)
+    public static void findOddEvenIndexSums(int[] array)
     {
-        int size = array.length;
-        int[] oddNumbers = new int[size/2];
-        int[] evenNumbers = new int[(int)(size/2 + 0.5)];
+        int oddSum = 0;
+        int evenSum = 0;
 
-        int index = 0;
+        boolean switcher = true;
 
         for(int i : array)
         {
-            if(i%2 == 0)
+            if(switcher)
             {
-                evenNumbers[index] = i;
+                evenSum += i;
             }
             else
             {
-                oddNumbers[index] = i;
+                oddSum += i;
             }
-            index++;
+            switcher = !switcher;
         }
 
-        int sumEven = sumAllNumbers(evenNumbers);
-        int sumOdd = sumAllNumbers(oddNumbers);
-
-        System.out.println("sum of odd numbered indexes: " + sumOdd);
-        System.out.println("sum of even numbered indexes: " + sumEven);
+        System.out.println("Odd numbered indexes total: " + oddSum);
+        System.out.println("Even numbered indexes total: " + evenSum);
     }
-
-    private static int sumAllNumbers(int[] array)
-    {
-        int sum = 0;
-        for(int i : array)
-        {
-            sum += i;
-        }
-        return sum;
-    }
+ 
 
 }

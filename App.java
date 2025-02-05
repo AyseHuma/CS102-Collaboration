0a07a86209ce378c26db732513fbc7c372b8984c
hale <hale.btl011@gmail.com>
2025-02-05 14:45:19 +0300

    Update App.java
    I added min and max methodes

--- App.java
+++ App.java
@@ -1,4 +1,29 @@
 public class App {
+     private static findMax()
+        {
+            int max = arr[0];
+            for(int num : arr)
+            {
+                if(max < num)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+
+        private static findMin()
+        {
+            int min = arr[0];
+            for(int num : arr)
+            {
+                if(min > num)
+                {
+                    min = num;
+                }
+            }
+            return min;
+        }
     public static void main(String[] args) {
         
     }

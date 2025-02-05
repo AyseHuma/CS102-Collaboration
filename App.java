b75de2daa7101f7c9f04f533abd65ac7a5d74d53
hale <hale.btl011@gmail.com>
2025-02-05 20:39:11 +0300

    Update App.java
    The methods were gone I added them back and made the private methods public.

--- App.java
+++ App.java
@@ -2,69 +2,98 @@ import java.util.Scanner;
 
 public class App {
     
-    public static void main(String[] args) 
+public static int findMax(int[] arr)
+        {
+            int max = arr[0];
+            for(int num : arr)
+            {
+                if(max < num)
+                {
+                    max = num;
+                }
+            }
+            return max;
+        }
+        public static int findMin(int[] arr)
+        {
+            int min = arr[0];
+            for(int num : arr)
+            {
+                if(min > num)
+                {
+                    min = num;
+                }
+            }
+            return min;
+        }
+    public static int [] createArray(int givenSize)
     {
-        Scanner in = new Scanner(System.in); 
-        int choice; 
-        do  
-        { 
-            System.out.printf("%s%n%s%n%s%n%s%n%s%n", "1. Find maximum number."  
-                                     , "2. Find minimum number." 
-                                     , "3. Find thw avarage of the array."  
-                                     , "4. Find the sum of elements with odd- and even-numbered indexes." 
-                                     , "5. Exit."); 
-            System.out.print("Please choose an option: "); 
-            choice = in.nextInt(); 
-            System.out.println(""); 
-            in.nextLine(); 
-            switch (choice)  
-            { 
-                case 1: 
-                    findMax(arr); 
-                    break; 
-                 
-                case 2: 
-                    findMin(arr); 
-                    break; 
-                case 3: 
-                    arrayAverage(arr); 
-                    break; 
-                case 4: 
-                    findOddEvenSumTotal(arr); 
-                    break; 
-                case 5: 
-                    System.out.println("Exiting program..."); 
-                    break; 
-                default: 
-                    break; 
-            } 
-        } while(choice != 5); 
-
+        Random rand = new Random();
+        int [] randomArray = new int [givenSize];
+        for(int i = 0 ; i < givenSize ; i ++)
+        {
+            randomArray [i] = rand.nextInt(101);
+        }
+        return randomArray;
     }
-
-    public static void findOddEvenIndexSums(int[] array)
+    public static void findOddEvenSumTotal(int[] array)
     {
-        int oddSum = 0;
-        int evenSum = 0;
+        int size = array.length;
+        int[] oddNumbers = new int[size];
+        int[] evenNumbers = new int[(int)(size)];
 
-        boolean switcher = true;
+        int index = 0;
 
         for(int i : array)
         {
-            if(switcher)
+            if(i%2 == 0)
             {
-                evenSum += i;
+                evenNumbers[index] = i;
             }
             else
             {
-                oddSum += i;
+                oddNumbers[index] = i;
             }
-            switcher = !switcher;
+            index++;
         }
 
-        System.out.println("Odd numbered indexes total: " + oddSum);
-        System.out.println("Even numbered indexes total: " + evenSum);
+        int sumEven = sumAllNumbers(evenNumbers);
+        int sumOdd = sumAllNumbers(oddNumbers);
+
+        System.out.println("sum of odd numbered indexes: " + sumOdd);
+        System.out.println("sum of even numbered indexes: " + sumEven);
     }
- 
+    public static void arrayAverage(int[] arr)
+     {
+        int sum = 0; 
+        for (int i = 0; i < arr.length; i++)
+        {
+            sum += arr[i];
+        }
+        int average = sum / arr.length; 
+        System.out.print("{");
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] - average);
+            if ( i != arr.length - 1)
+            {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
+    }
+    
+
+    public static int sumAllNumbers(int[] array)
+    {
+        int sum = 0;
+        for(int i : array)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
+
 
 }

74963b78fec6c82cdcd25af850635658e44440ab
doğa <doga.otcu@ug.bilkent.edu.tr>
2025-02-05 14:46:22 +0300

    Created an array with random integers

--- App.java
+++ App.java
@@ -1,5 +1,15 @@
+import java.util.Random;
 public class App {
     public static void main(String[] args) {
         
+    }
+    public static void createArray(int givenSize)
+    {
+        Random rand = new Random();
+        int [] randomArray = new int [givenSize];
+        for(int i = 0 ; i < givenSize ; i ++)
+        {
+            randomArray [i] = rand.nextInt(101);
+        }
     }
 }
